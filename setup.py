bf16d69645399f2f4340ff4f9fb12852735e7f66
Anze Slosar <slosar@gmail.com>
2016-12-05 10:04:54 -0500

    setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import distutils
+from distutils.core import setup
+
+description = "BMX raw file support"
+
+setup(name="bmxdaq", 
+      version="0.1.0",
+      description=description,
+      url="https://github.com/slosar/bmxdaq",
+      author="Anze Slosar",
+      py_modules=['bmxdata'],
+      package_dir={'': 'py'})
+
+
