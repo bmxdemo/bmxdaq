6a3f41ee601a839970f536f1e7e20a1b19a94249
Hindy <hindydrillick@gmail.com>
2017-08-02 15:46:18 -0400

    pragma once

--- daq/reduction.h
+++ daq/reduction.h
@@ -1,5 +1,6 @@
 //Parallel reduction algorithms to calculate different statistics over large arrays of numbers
 //for RFI detection and computing spectra
+#pragma once
 #include <cufft.h>
 #define OPS_PER_THREAD 8  //do OPS_PER_THREAD operations while loading into shared memory, to reduce the number of blocks needed
                           //must be power of 2!
