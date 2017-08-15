9373a47a9bdcc09883bfaa497b0c66b84c150826
Hindy <hindydrillick@gmail.com>
2017-08-15 16:05:57 -0400

    Modified default settings

--- daq/settings.cpp
+++ daq/settings.cpp
@@ -42,8 +42,8 @@ void init_settings(SETTINGS *s, char* fname) {
     s->fg_switchevery=10;
     sprintf(s->fg_port,"ttyS0");
     s->log_chunk_size = 20;
-    s->n_sigma_null = 3;
-    s->n_sigma_write = 3;
+    s->n_sigma_null = 0;
+    s->n_sigma_write = 0;
     s->use_mean_statistic = false;
     s->use_variance_statistic = false;
     s->use_abs_max_statistic = false;
