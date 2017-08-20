51fdb4cc99e8e0838265b13b5dd58384b5de8e0c
BMX user <anze@bnl.gov>
2017-08-20 12:49:15 -0400

    plot live test

--- py/plotLive.py
+++ py/plotLive.py
@@ -3,44 +3,68 @@ import bmxdata as bmx
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import sys
+import sys, glob, os, time
 from optparse import OptionParser
 
-parser = OptionParser()
 
-parser.add_option("--wf", dest="wf", default="",
-                  help="Plot waveform")
-parser.add_option("--interval", dest="interval", default=1000,
-                  help="plotting interval", type='int')
-parser.add_option("--psavg", dest="psavg", action="store_true",
-                  help="average ps")
-parser.add_option("--log", dest="log", action="store_true",
-                  help="plot log")
+def main():
+    ## need those globals for animate callback
+    global o, args, ax, fig, fname, d
+    o,args=getOpts()
+    ax,fig=initFig(o,args)
+    fname,d=initData(o,args)
+    # start animation
+    ani = animation.FuncAnimation(fig, animate, interval=o.interval)
+    plt.show()
 
-(o, args) = parser.parse_args()
+def getOpts():
 
-fig = plt.figure()
+    parser = OptionParser()
+    parser.add_option("--wf", dest="wf", action="store_true",
+                      help="Plot waveform. It will assume it lives in data/wave.bin")
+    parser.add_option("--interval", dest="interval", default=1000,
+                      help="plotting interval", type='int')
+    parser.add_option("--psavg", dest="psavg", action="store_true",
+                      help="average ps")
+    parser.add_option("--log", dest="log", action="store_true",
+                      help="plot log")
+    return parser.parse_args()
 
-ny=2
-nx=1
-if (len(o.wf)>0):
-    nx+=1
-    
-cc=0
-ax=[]
-for iy in range(ny):
-    ax.append([])
-    for ix in range(nx):
-        nax=fig.add_subplot(nx,ny,cc+1)
-        ax[-1].append(nax)
-        cc+=1
 
-d=bmx.BMXFile(args[0])
-print ax[1][0]
-print ax[0][0]
+def initFig(o,args):
+
+    fig = plt.figure()
+    ny=2
+    nx=1
+    if (o.wf>0):
+        nx+=1
+
+    cc=0
+    ax=[]
+    for iy in range(ny):
+        ax.append([])
+        for ix in range(nx):
+            nax=fig.add_subplot(nx,ny,cc+1)
+            ax[-1].append(nax)
+            cc+=1
+    return ax,fig
+
+def initData(o,args):
+    if len(args)>0:
+        fname=args[0]
+    else:
+        fname=max(glob.iglob('data/*.data'), key=os.path.getctime)
+
+    print "Reading ",fname
+    d=bmx.BMXFile(fname)
+    return fname,d
+
+
 def animate(i):
+    global fname,d
     nr=d.update(replace=not o.psavg)
     print "New records:",nr
+    
     if (nr>0):
         ax[0][0].clear()
         ax[1][0].clear()
@@ -49,9 +73,20 @@ def animate(i):
         if o.log:
             ax[0][0].semilogy()
             ax[1][0].semilogy()
+    else:
+        if (len(args)==0):
+            print "Looking for new file..."
+            fnamet=max(glob.iglob('data/*.data'), key=os.path.getctime)
+            if fnamet!=fname:
+                 print "Picked up ",fnamet
+                 time.sleep(2) ## wait for the file to start for real
+                 fname=fnamet
+                 d=bmx.BMXFile(fname)
 
-    if (len(o.wf)>0):
-        wfile = open(o.wf)
+
+
+    if (o.wf>0):
+        wfile = open("data/wave.bin")
         de=[('ch1','i1'),('ch2','i1')]
         da=np.fromfile(wfile,de)
         xx=np.arange(len(da))
@@ -60,10 +95,10 @@ def animate(i):
         ax[0][1].plot(xx,da['ch1'])
         ax[1][1].plot(xx,da['ch2'])
 
-ani = animation.FuncAnimation(fig, animate, interval=o.interval)
-plt.show()
 
 
-plt.show()
+if __name__=="__main__":
+    main()
+
 
 
