a9be851f7e1fc08604479fac1224180502e4d168
Hindy Drillick <hindydrillick@gmail.com>
2018-03-25 20:57:04 -0400

    Modified scripts

--- py/sync.sh
+++ py/sync.sh
@@ -5,5 +5,5 @@ for i in $(seq 1 $1); do
   rm $path/data/sync/*
   $path/daq/./daq.out $path/daq/sync.ini
   f=$(ls -Art $path/data/sync/ | tail -n 1) 
-  $path/py/./sync_test.py --f $path/data/sync/$f  --delays $path/py/$2 --peaks $path/py/$3
+  $path/py/./sync_test.py --f $path/data/sync/$f  --delays $path/py/$2 --peaks1 $path/py/$3 --peaks2 $path/py/$4 --nsamples 4
 done
--- py/sync_test.py
+++ py/sync_test.py
@@ -2,11 +2,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from optparse import OptionParser
+import sys
 
 parser = OptionParser()
 parser.add_option("--f", "--file", dest="fname")
-parser.add_option("--delays", dest="fdelays")
-parser.add_option("--peaks",  dest="fpeaks")
+parser.add_option("--delays", dest="fdelays", default = "std")
+parser.add_option("--peaks1",  dest="fpeaks1", default = "std")
+parser.add_option("--peaks2",  dest="fpeaks2", default = "std")
+parser.add_option("--ch", "--channel", dest ="channel", default = 'ch2')
+parser.add_option("--nsamples", dest="nsamples", default = 12, type="int")
+parser.add_option("--size", dest = "size", default = 2**27, type="int")
 
 (o, args) = parser.parse_args()
 
@@ -15,41 +20,45 @@ wfile = open(o.fname)
 dat_type=[('ch1','i1'),('ch2','i1')]
 data=np.fromfile(wfile,dat_type)
 
-nsamples = 12
-size = 2**27
-data = data['ch2']
+data = data[o.channel]
 
 peaks = [[], []] 
-for i in range(nsamples-1):
+for i in range(o.nsamples):
   for j in range(2):
     ##find where waveform starts peaking (20 should be large enough voltage)
-    a = np.where(data[(2*(i+1)+j)*size:(2*(i+1)+j+1)*size] > 20)
+    a = np.where(data[(2*(i)+j)*o.size:(2*(i)+j+1)*o.size] > 20)
     ##make sure sample isn't starting in middle of peak
     ##if i > 0:
-    b = max(data[(2*(i)+j+1)*size-1000:(2*(i)+j+1)*size])
+    ##  b = max(data[(2*(i)+j+1)*o.size-100000:(2*(i)+j+1)*o.size])
     ##else:
     ##  b = 0
-    if any(map(len,a)) and  b < 10:
-      peaks[j].append(np.min(a) + i*size)  
+    b = 0
+    if any(map(len,a)) and  b < 20:
+      peaks[j].append(np.min(a) + i*o.size)  
 
-delayFile = open(o.fdelays, 'a')
-peakFile = open(o.fpeaks, 'a')
+if o.fdelays == "std":
+  delayFile = sys.stdout
+else:
+  delayFile = open(o.fdelays, 'a')
+if o.fpeaks1 == "std":
+  peakFile = [sys.stdout, sys.stdout]
+else:
+  peakFile = [open(o.fpeaks1, 'a'), open(o.fpeaks2, 'a')]
 
 for i in range(min(len(peaks[0]), len(peaks[1]))):
   delay = peaks[0][i] - peaks[1][i]
   delay = (1.0 * delay)/(1100000)
-  delayFile.write(str(delay))
-  delayFile.write(" ")
+  delayFile.write(str(delay) + " ")
 
 delayFile.write("\n")
 
 for i in range(2):
   for n in peaks[i]:
     time = 1.0*n/1100000
-    peakFile.write(str(time))
-    peakFile.write(" ")
-  peakFile.write("\n")
+    peakFile[i].write(str(time) + " ")
+  peakFile[i].write("\n")
    
 
 delayFile.close()
-peakFile.close()
+peakFile[0].close()
+peakFile[1].close()
